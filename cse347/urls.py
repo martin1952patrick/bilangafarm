c91849f7dae53159606fc362a77a6925cd346e44
martin1952patrick <martinpati1952@gmail.com>
2024-01-23 15:59:36 +0300

    first commit

---
+++ cse347/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from bdagro import views 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path('accounts/', include('user.urls')),
+    path('blog/',  include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('projects/', include('bdagro.urls')),
+    path('statistic/', include('statistic.urls')),
+    path('wings/', include('wings_app.urls')),
+    path('news/', include('info_app.urls')), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [ 
+    path('signup/', signup_view, name="signup"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+    path('profile/', profile_view, name="profile"),
+    path('profile_update/', profile_update_view, name="profile_update"),
+    
+]
